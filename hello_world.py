dc18d5f199c52b7ac7ec3d80dcf0f9b8e3ce538c
Andrew McGuire <mcguirea@gmail.com>
2014-10-14 18:27:48 -0700

    V1

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/hello/<name>")
+def hello_person(name): 
+	html = """
+		<h1>
+			Hello {}!
+		</h1>
+		<p>
+			Here's a picture of a kitten.  Awww...
+		</p>
+		<img src = "http://placekitten.com/200/300">
+	"""
+
+	return html.format(name.title())
+
+if __name__ == "__main__": 
+	app.run()
+
